6a5c879b20fa48b7bfa148bc893f8887c1a2bce5
yin-lo <yinlo@hotmail.fr>
2024-04-09 14:06:35 +0200

    envoie des données levels à la vue level

--- app/controllers/levelController.js
+++ app/controllers/levelController.js
@@ -1,37 +1,42 @@
-const dataMapper = require("../database/dataMapper")
-const Level = require("../models/Active Record/Level")
+// const dataMapper = require("../database/dataMapper")
+// const Level = require("../models/Active Record/Level")
+const { Level } = require('../models/Sequelize/Association');
 
 const levelController = {
-    displayLevels: async (req, res) => {
-        // Toutes les lignes du try vont etre executees tant qu'on ne rencontre pas d'erreur
-        try {
+	displayLevels: async (req, res) => {
+		// Toutes les lignes du try vont etre executees tant qu'on ne rencontre pas d'erreur
+		try {
+			const levels = await Level.findAll();
 
-            // Recuperer la liste de mes levels
-            // const levels = await dataMapper.getAllLevels()
-            const levels = await Level.getAll();
+            const levelsWithQuestions = await Level.findAll({
+				include: 'levelQuestions',
+			});
 
-            res.render('levels', { levels })
-        } catch (error) {
-            // Si la moindre erreur se produit alors on rentre dans le catch
-            console.log(error.message)
-            res.status(500).render('500')
-        }
-    },
+            console.log(levelsWithQuestions.map(leveWithQuestions => leveWithQuestions.toJSON()));
 
-    addLevel: async (req, res) => {
-        try {
-            // await dataMapper.createLevel({ name: req.body.name })
-            // Avec le design patern active record on va d'abord instancier un objet grace a notre classe
-            const newLevel = new Level({ name: req.body.name })
-            // puis appeler la methode create
-            await newLevel.create()
-            res.redirect('/levels')
-        } catch (error) {
-            // Si la moindre erreur se produit alors on rentre dans le catch
-            console.log(error.message)
-            res.status(500).render('500')
-        }
-    }
-}
+
+			res.render('levels', { levels: levelsWithQuestions });
+		} catch (error) {
+			// Si la moindre erreur se produit alors on rentre dans le catch
+			console.log(error.message);
+			res.status(500).render('500');
+		}
+	},
+
+	addLevel: async (req, res) => {
+		try {
+			// await dataMapper.createLevel({ name: req.body.name })
+			// Avec le design patern active record on va d'abord instancier un objet grace a notre classe
+			const newLevel = new Level({ name: req.body.name });
+			// puis appeler la methode create
+			await newLevel.create();
+			res.redirect('/levels');
+		} catch (error) {
+			// Si la moindre erreur se produit alors on rentre dans le catch
+			console.log(error.message);
+			res.status(500).render('500');
+		}
+	},
+};
 
 module.exports = levelController;
